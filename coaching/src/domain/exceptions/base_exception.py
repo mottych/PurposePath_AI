a40a97cc1e680a88f35d991b0dee73514d65ce6c
Motty Chen <motty@gmail.com>
2025-10-27 19:42:23 -0500

    fix: add noqa for backwards compat alias

--- coaching/src/domain/exceptions/base_exception.py
+++ coaching/src/domain/exceptions/base_exception.py
@@ -79,5 +79,5 @@ class DomainError(Exception):
 
 __all__ = ["DomainError"]
 
-# Backwards compatibility alias (deprecated)
+# Backwards compatibility alias (deprecated)  # noqa: N818
 DomainException = DomainError
