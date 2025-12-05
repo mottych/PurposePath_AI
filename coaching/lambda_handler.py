43175df0587d247d2e34a613adca75e336fe38f0
Motty Chen <motty@xirius.com>
2025-12-05 17:19:57 -0600

    fix(#116): export lambda_handler wrapper instead of raw Mangum handler

--- coaching/lambda_handler.py
+++ coaching/lambda_handler.py
@@ -1,6 +1,6 @@
 """Lambda handler - imports from coaching.src.api.main."""
 
-from coaching.src.api.main import handler
+from coaching.src.api.main import lambda_handler as handler
 
 # Re-export for Lambda
 __all__ = ["handler"]
